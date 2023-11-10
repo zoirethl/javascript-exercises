995620f3ccf515a1f527a2b5157f5153b39faba1
Zoireth Liendo <37812914+zoirethl@users.noreply.github.com>
2023-11-09 19:34:34 -0500

    second commit

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(numA, numB) {
+	let res = numA + numB;
+  return res;
 };
 
-const subtract = function() {
-	
+const subtract = function(numA, numB) {
+	let res = numA - numB;
+  return res;
 };
 
-const sum = function() {
-	
+const sum = function(numN) {
+	let res = numN.reduce((numA, numB) => numA + numB, 0);
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function(numN) {
+  let res = numN.reduce((numA, numB) => numA * numB)
+  return res;
 };
 
-const power = function() {
-	
+const power = function(numA, numB) {
+    let res = Math.pow(numA,numB);
+    return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(numA) {
+  if (numA === 0) return 1;
+  let res = 1;
+  for (let i = numA; i > 0; i--) {
+    res *= i;
+  }
+	return res;
+  
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const re = /[\W_]/g;
+    const lowRedStr = str.toLowerCase().replace(re, '');
+    const reverseStr = lowRedStr.split('').reverse().join('');
+    return (lowRedStr === reverseStr)
 };
 
 // Do not edit below this line
